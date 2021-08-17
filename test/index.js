7cbdf77d06c6885ef4563edea52b5f4e16b33afa
禹喆 <yuzhe.ljj@alibaba-inc.com>
2021-08-17 20:27:06 +0800

    引号

--- test/index.js
+++ test/index.js
@@ -1,25 +1,25 @@
-const co = require("co");
-const xtpl = require("xtpl");
-const fs = require("fs");
-const thunkify = require("thunkify");
-const path = require("path");
-const prettier = require("prettier");
-const { NodeVM } = require("vm2");
-const _ = require("lodash");
-const data = require("./animation");
-const componentsMap = require("./componentsMap");
-const helper = require("@imgcook/dsl-helper");
+const co = require('co');
+const xtpl = require('xtpl');
+const fs = require('fs');
+const thunkify = require('thunkify');
+const path = require('path');
+const prettier = require('prettier');
+const { NodeVM } = require('vm2');
+const _ = require('lodash');
+const data = require('./animation');
+const componentsMap = require('./componentsMap');
+const helper = require('@imgcook/dsl-helper');
 
 const vm = new NodeVM({
-  console: "inherit",
+  console: 'inherit',
   sandbox: {},
 });
 
 co(function*() {
   const xtplRender = thunkify(xtpl.render);
   const code = fs.readFileSync(
-    path.resolve(__dirname, "../src/index.js"),
-    "utf8"
+    path.resolve(__dirname, '../src/index.js'),
+    'utf8'
   );
   const renderInfo = vm.run(code)(data, {
     prettier: prettier,
@@ -34,7 +34,7 @@ co(function*() {
 
   if (renderInfo.noTemplate) {
     renderInfo.panelDisplay.forEach((file) => {
-      console.log("fileName", file.panelName);
+      console.log('fileName', file.panelName);
       // console.log("value", file.panelValue);
       fs.writeFileSync(
         path.join(__dirname, `../code/${file.panelName}`),
@@ -44,7 +44,7 @@ co(function*() {
   } else {
     const renderData = renderInfo.renderData;
     const ret = yield xtplRender(
-      path.resolve(__dirname, "../src/template.xtpl"),
+      path.resolve(__dirname, '../src/template.xtpl'),
       renderData,
       {}
     );
@@ -55,6 +55,6 @@ co(function*() {
 
     const prettierRes = prettier.format(ret, prettierOpt);
 
-    fs.writeFileSync(path.join(__dirname, "../code/result.js"), prettierRes);
+    fs.writeFileSync(path.join(__dirname, '../code/result.js'), prettierRes);
   }
 });
